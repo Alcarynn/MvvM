edb1fd2e8b6ac47b83c0beb76c5ca6eacba57b1d
gheraibia <gheraibia@hotmail.com>
2018-08-27 22:17:22 +0200

    Add files via upload

---
+++ hello/src/com/alex/poe/rest/HelloWorldRest.java
@@ -0,0 +1,29 @@
+package com.alex.poe.rest;
+
+import com.alex.poe.media.Book;
+
+import javax.ws.rs.core.*;
+
+import javax.ws.rs.*;
+
+@Path("/hello")
+public class HelloWorldRest {
+
+    @GET
+    @Path("/world")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getHello() {
+        return "Hello World!";
+    }
+
+    @GET
+    @Path("/book")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Book getBook() {
+        Book b = new Book();
+        b.setTitle("JSON");
+        b.setId(12);
+        b.setPrice(99);
+        return b;
+    }
+}
